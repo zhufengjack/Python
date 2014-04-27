c97de238fcc715ddb0a58b0ee842466ce89dd090
Jack Zhu <zhufengjack@gmail.com>
2014-04-27 12:24:43 +0800

    Add new files

--- moviesnested.py
+++ moviesnested.py
@@ -2,4 +2,5 @@ import nester
 movies=["The Holy Grails",1975,"Terry Jones & Terry Gillian",91,
           ["Graham Chapman",["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry Jones"]]]
 
-nester.print_lol(movies);
+#nester.print_lol(movies);
+nester.print_lol(movies,0);
--- nester.py
+++ nester.py
@@ -2,9 +2,8 @@
 This module is used to loop list and print all item in list inclued nested list.
 '''
 
-def print_lol(the_list):
+'''def print_lol(the_list):
 
-  '''
   (the_list)->None
 
   This function is used to loop all items in the_list and print them recursively
@@ -16,10 +15,33 @@ def print_lol(the_list):
   28
   Male
 
-  '''
+
 
   for each_item in the_list:
     if(isinstance(each_item,list)):
       print_lol(each_item);
     else:
+      print(each_item);
+'''
+
+
+def print_lol(the_list,indent=False,level=0):
+  '''
+  (List,Integer)-->None
+
+  loop all items in the_list and print all item recursively.
+
+  >>>Student=[28,'Male',["Jack","Zhu"]]
+  28
+  Male
+    Jack
+    Zhu
+  '''
+  for each_item in the_list:
+    if(isinstance(each_item,list)):
+      print_lol(each_item,indent,level+1);
+    else:
+      if(indent):
+        for tab_stop in range(level):
+          print("\t",end='');
       print(each_item);
