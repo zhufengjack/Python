017adc2a1e22c87ecb4218a005db1f8c82e4f748
Jack Zhu <zhufengjack@gmail.com>
2014-04-27 17:41:09 +0800

    add helloworld project

---
+++ helloworld/helloworld.py
@@ -0,0 +1,13 @@
+"""A simple webapp2 server."""
+
+import webapp2
+
+
+class MainPage(webapp2.RequestHandler):
+
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World! Python can work for this system')
+
+
+application = webapp2.WSGIApplication([('/', MainPage),], debug=True)
